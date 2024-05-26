dcbb07978dab267edfee088daf5cd113290c44ca
Konstantin Maurin <artsjesus7@gmail.com>
2024-05-26 15:26:38 +0300

    optimization

--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,28 @@ def load_operation():
         operation["date"] = parser.parse(operation["date"]).date()
         operations.append(operation)
     return operations
+
+
+def number_card(operations_name_card, operations_number_card):
+    """Вывод номера карты или счета в нужном формате"""
+    operations_number_card = list(operations_number_card)
+    if operations_name_card != "Счет ":
+        for index, item in enumerate(operations_number_card):
+            if 5 < index < 12:
+                operations_number_card[index] = "*"
+        operations_number_card.insert(4, " ")
+        operations_number_card.insert(9, " ")
+        operations_number_card.insert(14, " ")
+        operations_number_card.insert(19, " ")
+        operations_number_card = "".join(operations_number_card)
+        return operations_number_card
+    else:
+        operations_number_card = "".join(operations_number_card)
+        operations_number_card = "**" + operations_number_card[-4:]
+        return operations_number_card
+
+
+
+
+
+
