7b57ca8f45aaf9c7f113413811f65a4109c37a2f
Konstantin Maurin <artsjesus7@gmail.com>
2024-05-26 11:43:34 +0300

    функция load_operation

---
+++ project3/utils.py
@@ -0,0 +1,18 @@
+import json
+from dateutil import parser
+
+
+def load_operation():
+    operations = []
+    with open("operations.json", "r", encoding="UTF-8") as file:
+        operation_list = [operation for operation in json.load(file) if operation and operation["state"] == "EXECUTED"]
+    operation_list.sort(key=lambda x: str(x.get("date")), reverse=True)
+    operation_list = operation_list[:5]
+
+    for operation in operation_list:
+        operation["date"] = parser.parse(operation["date"]).date()
+        operations.append(operation)
+    return operations
+
+
+
