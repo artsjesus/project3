579ced5419418a9ac49b3473b4e2aa9912726b8e
Konstantin Maurin <artsjesus7@gmail.com>
2024-05-26 15:29:24 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -12,13 +12,13 @@ def main():
         if "from" in operation:
             operations_name_card = re.sub("[^a-zA-ZА-Яа-я\s]", "", operation["from"])
             operations_number_card = re.sub("[\D]", "", operation["from"])
-            card_output = number_card(operations_name_card,operations_number_card)
+            card_output = number_card(operations_name_card, operations_number_card)
             print(f"{operation_date}, {operation["description"]}\n"
                   f"{operations_name_card}{card_output}->{operations_name_to}{operations_output_to}\n"
                   f"{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}\n")
         else:
             print(f"{operation_date}, {operation["description"]}\n"
-                  f"{operations_name_to} {operations_output_to}\n"
+                  f"{operations_name_to}{operations_output_to}\n"
                   f"{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}\n")
 
 
